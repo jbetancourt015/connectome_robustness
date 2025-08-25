d377ee8551154227ce8b2a8dd8ec08e042f4adbd
Jose Betancourt <jose.betancourtvalencia@yale.edu>
2025-08-25 12:44:47 -0700

    Modified color of simulated loss figure

--- code/connectome_error_simulation.py
+++ code/connectome_error_simulation.py
@@ -20,6 +20,7 @@ import network_functions
 import numpy as np
 import matplotlib.pyplot as plt
 import logging
+from matplotlib.colors import LinearSegmentedColormap
 from numba import njit
 from tqdm import tqdm
 
@@ -50,6 +51,13 @@ data_idx = 5
 # Plotting colors
 con_colors = np.array([[0, 77, 128], [181, 23, 0], [1, 113, 0], [242, 112, 0], 
                    [120, 0, 150], [255, 204, 0], [203, 41, 123], [0, 0, 0]])/255;
+
+def fade_to_color_cmap(rgb, alpha_min, name="fade_to_color"):
+    bottom = (*rgb, alpha_min)
+    top    = (*rgb, 1.0)
+    return LinearSegmentedColormap.from_list(name, [bottom, top], N=256)
+
+cmap=fade_to_color_cmap(con_colors[0], alpha_min=0.2, name="fade_to_color")
 
 # Get connectome
 A = network_functions.load_connectome(data_idx)
@@ -123,7 +131,7 @@ plt.xlabel('Connection strength')
 plt.ylabel('Probability')
 plt.xscale('log')
 plt.yscale('log')
-plt.savefig('../figures/simulations/sampled_weight_dist.pdf', dpi=600, bbox_inches='tight')
+plt.savefig('../../figures/simulations/sampled_weight_dist.pdf', dpi=600, bbox_inches='tight')
 plt.show()
 
 # Generate raw plot
@@ -132,7 +140,7 @@ fig, ax = plt.subplots(figsize=(.9*width, .9*height))
 ax.scatter(s_weights, P_weights, color=con_colors[0], rasterized=True)
 ax.set_xscale('log')
 ax.set_yscale('log')
-plt.savefig('../raw_figures/simulations/sampled_weight_dist.pdf', dpi=600)
+plt.savefig('../../raw_figures/simulations/sampled_weight_dist.pdf', dpi=600)
 plt.show()
 
 #------------------------------------------------------------------------------
@@ -141,7 +149,7 @@ plt.show()
 # Initialize loss and parameter vectors
 loss = []
 mean = []
-var = []
+m2 = []
 
 # Perturbation parameters
 eta = 1.
@@ -162,33 +170,34 @@ for i, w in enumerate(tqdm(incoming_weights)):
     # Save loss and vector statistics
     loss.append(average_error(x,w,w_hat,1.))
     mean.append(np.mean(w))
-    var.append(np.var(w))
+    m2.append(np.mean(w**2))
 
 # Convert to numpy arrays
 loss = np.array(loss)
 mean = np.array(mean)
-var = np.array(var)
+m2 = np.array(m2)
 
 # Get variance range
-min_var, max_var = min(var), max(var)
+min_m2, max_m2 = min(m2), max(m2)
 min_mean, max_mean = min(mean), max(mean)
 
 n_plot = 100
 mean_vals = np.linspace(min_mean, max_mean, n_plot)
 
 # Separate high and low variance values
-high_mask = (var > np.median(var))
-low_mask = (var <= np.median(var))
+high_mask = (m2 > np.median(m2))
+low_mask = (m2 <= np.median(m2))
 
 # Compare with model predictions
 fig, ax = plt.subplots(figsize=(.9*width, .9*height))
-ax.scatter(mean[high_mask], loss[high_mask], color=con_colors[1], rasterized=True)
-ax.scatter(mean[low_mask], loss[low_mask], color=con_colors[0], rasterized=True)
-ax.plot(mean_vals, np.arccos((1+(eps**2)*(mean_vals/(max_var + mean_vals**2)))**(-1/2))/np.pi, c=con_colors[1])
-ax.plot(mean_vals, np.arccos((1+(eps**2)*(mean_vals/(min_var + mean_vals**2)))**(-1/2))/np.pi, c=con_colors[0])
+# ax.scatter(mean[high_mask], loss[high_mask], color=con_colors[1], rasterized=True)
+# ax.scatter(mean[low_mask], loss[low_mask], color=con_colors[0], rasterized=True)
+ax.scatter(mean, loss, c=m2, cmap=cmap)
+ax.plot(mean_vals, np.arccos((1+(eps**2)*(mean_vals/max_m2))**(-1/2))/np.pi, c=con_colors[1])
+ax.plot(mean_vals, np.arccos((1+(eps**2)*(mean_vals/min_m2))**(-1/2))/np.pi, c=con_colors[0])
 ax.set_xscale('log')
 ax.set_yscale('log')
-plt.savefig(f"../raw_figures/simulations/loss_vs_mean_eta_{int(eta)}.pdf", dpi=600)
+plt.savefig(f"../../raw_figures/simulations/loss_vs_mean_eta_{int(eta)}.pdf", dpi=600)
 plt.show()
 
 
